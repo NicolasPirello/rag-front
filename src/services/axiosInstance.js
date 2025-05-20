48ae68d36153178cbe5f98534e7833d8845b09ba
NicolasPirello <nicoalepirello@gmail.com>
2025-05-20 20:23:00 -0300

    Cambios de clave

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "https://apirag.nicolaspirello.com/";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://apirag.nicolaspirello.com";
+const API_KEY = import.meta.env.VITE_API_KEY || "s3cret-super-safe"; // ¡no dejar este hardcodeado en producción!
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
+    "X-APP-AUTH": API_KEY,
   },
 });
 
