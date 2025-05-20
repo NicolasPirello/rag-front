135a81c8f92be99eb155c3faa8a915b33390f328
NicolasPirello <nicoalepirello@gmail.com>
2025-05-20 19:59:37 -0300

    Arreglo de API hasta poner ENV

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://apirag.nicolaspirello.com/";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
